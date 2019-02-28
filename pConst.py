d7cb43d9f828f796a64f9cb1a1dabbb8f351cc34
mihhy <moto38@gmail.com>
2019-02-28 23:31:01 +0900

    Create pConst.py

---
+++ pConst.py
@@ -0,0 +1,22 @@
+#Note:The template file will be copied to a new file. When you change the code of the template file you can create new file with this base code. 
+
+# part
+partBody=0 # (body all)
+partHead=1
+partHoppe=2
+partRHand=3
+partLHand=4
+partStoma=5
+partHip=6
+
+# how
+touchLong=0
+touch1=1
+touch2=2
+
+#sleep,kaze,kigen,warau,game
+statSleep=0
+statKaze=1
+statKigen=2
+statWarau=3
+statGame=4
